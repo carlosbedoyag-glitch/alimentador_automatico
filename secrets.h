1b59dc5802179bffba99eb786760f8945de29bc2
carlosbedoyag-glitch <carlos.bedoyag@est.iudigital.edu.co>
2026-09-23 05:47:51 -0500

    Add secrets template for Wi-Fi and Ubidots credentials

---
+++ secrets.h
@@ -0,0 +1,9 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+const char *WIFI_SSID = "TU_SSID";
+const char *WIFI_PASS = "TU_PASSWORD";
+const char *UBIDOTS_TOKEN = "TU_UBIDOTS_TOKEN";
+const char *DEVICE_LABEL = "control-termico-esp32";
+
+#endif
