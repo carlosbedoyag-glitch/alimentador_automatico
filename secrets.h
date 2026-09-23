42339f0de6132b6916a9e1b03a4553ca7fbdeabd
carlosbedoyag-glitch <carlos.bedoyag@est.iudigital.edu.co>
2026-09-23 06:07:44 -0500

    Delete secrets.h

--- secrets.h
+++
@@ -1,9 +0,0 @@
-#ifndef SECRETS_H
-#define SECRETS_H
-
-const char *WIFI_SSID = "TU_SSID";
-const char *WIFI_PASS = "TU_PASSWORD";
-const char *UBIDOTS_TOKEN = "TU_UBIDOTS_TOKEN";
-const char *DEVICE_LABEL = "control-termico-esp32";
-
-#endif
